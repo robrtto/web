79fc677464b2b75bcc81708d1fd96a00ca3fffb2
robrtto <robertogabrielortiz254@gmail.com>
2024-11-07 01:03:20 -0800

    add text

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('open');
+});
+
+const video = document.getElementById('intro-video');
+const buttonContainer = document.querySelector('.portfolio-button-container');
+
+video.onended = () => {
+    buttonContainer.computedStyleMap.opacity = 1;
+};
+
+function enterSize() {
+    window.location.href = "main-site.html"
+}
+
