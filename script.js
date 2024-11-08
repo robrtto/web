1e677238211d87c567c195fd86ea35d922ceac29
robrtto <robertogabrielortiz254@gmail.com>
2024-11-08 00:41:43 -0800

    change everything

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
-document.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
-
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('.nav-menu').classList.toggle('open');
-});
-
-const video = document.getElementById('intro-video');
-const buttonContainer = document.querySelector('.portfolio-button-container');
-
-video.onended = () => {
-    buttonContainer.computedStyleMap.opacity = 1;
-};
-
-function enterSite() {
-    window.location.href = "main.html"
-}
 function toggleMenu() {
-    const menu = document.querySelector('.nav-menu');
+    const menu = document.getElementById('nav-menu');
     menu.classList.toggle('open');
 }
 
+function enterSite() {
+    window.location.href = 'main.html';
+}
